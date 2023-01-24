7e98e4d926d8968480fc57d53826c7456b8658b1
Crowdbotics <team@crowdbotics.com>
2023-01-24 16:28:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "birthday-calculator-38428",
+  url: "https://birthday-calculator-38428.botics.co",
+  api: "https://birthday-calculator-38428.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
